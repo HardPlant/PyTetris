6cf54c35469b23034d0822f4fa2bd8856a9462fa
HardPlant <abc7988se@naver.com>
2017-09-29 18:25:15 +0900

    Shape Controller

---
+++ Shape.py
@@ -0,0 +1,29 @@
+class Shape(object):
+    def __init__(self):
+        pass
+    def shape(self):
+        pass
+    def setShape(self):
+        pass
+    def setRandomShape(self):
+        pass
+    def x(self, index):
+        pass
+    def y(self, index):
+        pass
+    def setX(self, index, x):
+        pass
+    def setY(self, index, y):
+        pass
+    def minX(self):
+        pass
+    def maxX(self):
+        pass
+    def minY(self):
+        pass
+    def maxY(self):
+        pass
+    def rotatedLeft(self):
+        pass
+    def rotatedRight(self):
+        pass
